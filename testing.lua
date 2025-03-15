a7a18fa0c59757f0bfaec9e8d6a4ff9614524361
gaesanz <148149106+gaesanz@users.noreply.github.com>
2025-03-16 03:01:40 +0700

    Create testing.lua

---
+++ testing.lua
@@ -0,0 +1,15 @@
+SendVariantList({[0] = "OnDialogRequest",
+[1] =
+"\nadd_label_with_icon|big|`wSocial Portal````|left|1366|"..
+"\nadd_spacer|small|"..
+"\nadd_button|friendlists|`4LELANG |noflags|0|0|"..
+"\nadd_button|friendlist|`wShow Friends``|noflags|0|0|"..
+"\nadd_button|debts|`wShow Debts``|noflags|0|0|"..
+"\nadd_button|leaderboard|`wShow Leaderboard``|noflags|0|0|"..
+"\nadd_button|guild|`wCreate Guild``|noflags|0|0|"..
+"\nadd_button|trades|`wTrade History``|noflags|0|0|"..
+"\nadd_button|globaltrades|`wGlobal Trades``|noflags|0|0|"..
+"\nadd_button|activity|`wActivity Hub``|noflags|0|0|"..
+"\nadd_button|lottery|`9The Lottery``|noflags|0|0|"..
+"\nadd_quick_exit|"..
+"\nend_dialog|social|OK||"})
